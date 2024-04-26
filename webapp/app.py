15f057cf5835b1d52ac489fc41cd4e041f14b914
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-26 04:24:08 +0200

    data population instance to initiate session for glob var swimmers

--- webapp/app.py
+++ webapp/app.py
@@ -11,23 +11,26 @@ app.secret_key = "12345"
 def index():
     return "This is a placeholder for your webapp's index page"
 
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        swim_files.remove(".DS_Store")
+        session["swimmers"] = {}
+
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
+
 @app.get("/swimmers")
 def display_swimmers():
-    swim_files = os.listdir(swimclub.FOLDER)
-    swim_files.remove(".DS_Store")
-
-    session["swimmers"]={}
-
-    for file in swim_files:
-        name, *_ = swimclub.read_swim_data(file)
-        if name not in session["swimmers"]:
-            session["swimmers"][name] = []
-        session["swimmers"][name].append(file)
-    
+    populate_data()
     return str(sorted(session["swimmers"]))
 
 @app.get("/files/<swimmer>")
 def get_swimmers_files(swimmer):
+    populate_data()
     return str(session["swimmers"][swimmer])
 
 if __name__ == "__main__":
