8e22b7689ecbd96807d16cedcd27d934cc624c62
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-04-26 04:09:45 +0200

    Internal server error 505 for name search, fixed using Flask session sharing

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, session
 
 import os
 import swimclub
 
+
 app = Flask(__name__)
+app.secret_key = "12345"
 
 @app.get("/")
 def index():
@@ -14,16 +16,19 @@ def display_swimmers():
     swim_files = os.listdir(swimclub.FOLDER)
     swim_files.remove(".DS_Store")
 
-    swimmers={}
+    session["swimmers"]={}
 
     for file in swim_files:
         name, *_ = swimclub.read_swim_data(file)
-        if name not in swimmers:
-            swimmers[name] = []
-        swimmers[name].append(file)
+        if name not in session["swimmers"]:
+            session["swimmers"][name] = []
+        session["swimmers"][name].append(file)
     
-    return str(sorted(swimmers))
+    return str(sorted(session["swimmers"]))
 
+@app.get("/files/<swimmer>")
+def get_swimmers_files(swimmer):
+    return str(session["swimmers"][swimmer])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
